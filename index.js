5a223a8de13725ac87e160385ee430d7c00b16fc
Kaushal Mehra <kaushalmehra1987@gmail.com>
2021-01-12 22:51:49 +0530

    first query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import schema from './schema'
+import { graphqlHTTP } from 'express-graphql'
 
 const app = express();
 
@@ -6,5 +8,21 @@ const app = express();
 app.get('/', (req, res)=>{
     res.send('Graphql is amazing');
 })
+
+const root = { friend: () => {
+    return {
+        "id": 123456,
+        "firstName": "kaushal",
+        "lastName": "Mehra",
+        "gender": "male",
+        "email": "a@blur.com"
+    }
+}}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 
 app.listen(8000, ()=>{console.log("Running on 8080 port")});
